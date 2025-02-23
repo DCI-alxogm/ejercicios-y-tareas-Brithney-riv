92e46cd62c9bc061bce37ef39487983b41f250eb
Brithney_riv <bg.maciasrivera@ugto.mx>
2025-02-23 15:52:40 -0600

    Segundo programa de clase con Float

--- Feb17/operaciones_float.c
+++ Feb17/operaciones_float.c
@@ -5,19 +5,20 @@
 int main()
 {
    float a,b,c,d;
-   int e1, e2, e3, e4;
+   float e1, e2, e3, e4;
 
-  a=-9;
-  b=7;
-  c=3;
-  d=-2;
+  a=3.1416;
+  b=-8.09;
+  c=0.45;
+  d=-2.06;
   e1=(a+b)*c/d;
   e2=((a+b)*c)/d;
   e3=(a+b)*(c/d);
   e4=a+b*c/d;
+ 
+  printf("e=(a+b)*c/d=%f\n",e1);
+  printf("e=((a+b)*c)/d=%f\n", e2);
+  printf("e=(a+b)*(c/d)=%f\n", e3);
+  printf("e=(a+b*c/d=%f\n", e4);
 
-  printf("e=(a+b)*c/d=%d\n",e1);
-  printf("e=((a+b)*c)/d=%d\n", e2);
-  printf("e=(a+b)*(c/d)=%d\n", e3);
-  printf("e=(a+b*c/d=%d\n", e4);
-}
+ }
