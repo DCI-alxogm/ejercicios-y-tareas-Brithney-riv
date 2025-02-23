160e34c3e57defc0c1c7efc292f4d9ba85f0a4e7
Brithney_riv <bg.maciasrivera@ugto.mx>
2025-02-23 17:59:52 -0600

    Tarea utilizando variables float: números reales

--- Feb17/Tarea_float.c
+++ Feb17/Tarea_float.c
@@ -4,9 +4,10 @@
 
 int main()
 {
-   float a,b,c,d;   //Variables
-   float e1, e2, e3, e4;
-
+   float a,b,c,d;  //Variables
+   float e1, e2, e3, e4;   
+   
+  //Instrucciones al usuario
   printf("introduce el valor de a:");
   scanf("%f",&a);
   printf("introduce el valor de b:");
