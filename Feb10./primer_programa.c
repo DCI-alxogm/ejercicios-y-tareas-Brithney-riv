57be31187e0cb53239ade9de10d82977495b0a36
Brithney_riv <bg.maciasrivera@ugto.mx>
2025-03-19 01:06:32 -0600

    Este es el programa de ejemplo del curso

---
+++ Feb10./primer_programa.c
@@ -0,0 +1,9 @@
+//*Creado el 10 de Febrero de 2025 por Brithney GUadalupe Macias Rivera*/ 
+#include<stdio.h> 
+int main() 
+{ 
+ 
+printf("Hola Brithney \n"); 
+printf("Este es el primer programa del curso\n"); 
+} 
+/*Termine de escribir el programa*/
