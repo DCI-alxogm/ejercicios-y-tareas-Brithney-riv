00535b467b9c6ace3391ca8102a9f2a145af5103
Brithney_riv <bg.maciasrivera@ugto.mx>
2025-03-19 02:03:18 -0600

    Ejercicio de clase con switch

--- Mar10/Tem_Switch.c
+++ Mar10/Tem_Switch.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
- 
+
 int main (){
-     char op;
-     float valor, resultado;
+char op;
+float valor, resultado;
 
-     printf("Selecciona que deseas realizar: \n");
-     printf(" 'a' para convertir de °C a K \n");
-     printf(" 'b' para convertir de  K a °C \n");
-     scanf("%c", &op);
+ printf("Selecciona que deseas realizar: \n");
+ printf(" 'a' para convertir de Â°C a K \n");
+ printf(" 'b' para convertir de  K a Â°C \n");
+ scanf("%c", &op);
 
-     printf("Ingrese el valor de su temperatura: \n");
-     scanf("%f", &valor);     
-            
-           switch(op) {
-            case 'a': //Convertir celsius a Kelvin
-                      resultado = valor + 273.15;
-                      printf("%.2f °C, es igual a %.2f K \n");
-                      break:
-            case 'b': //Convertir Kelvin a Celsius
-                      resultado = valor - 273.15;
-                      printf("%.2f K, es igual a %.2f °C \n", valor, resultado);
-                      break:
-        
-            default:
-                   printf("Opción no valida \n");
-                   break;
-                           }
-      printf("Fin \n");
-      return(0);
+ printf("Ingrese el valor de su temperatura: \n");
+ scanf("%f", &valor);     
+
+       switch(op) {
+        case 'a': //Convertir celsius a Kelvin
+                  resultado = valor + 273.15;
+                  printf("%f Â°C, es igual a %f K \n", valor, resultado);
+                  break;
+        case 'b': //Convertir Kelvin a Celsius
+                  resultado = valor - 273.15;
+                  printf("%f K, es igual a %f Â°C \n", valor, resultado);
+                  break;
+
+        default:
+               printf("Opcion no valida \n");
+               break;
+                   }
+  printf("Fin \n");
+  return(0);
 }
