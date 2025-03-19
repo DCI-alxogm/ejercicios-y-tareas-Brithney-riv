da04f95941957459a01b1c73471b4445e1fc0c85
Brithney_riv <bg.maciasrivera@ugto.mx>
2025-03-19 01:04:43 -0600

    Este es el primer programa del curso

---
+++ Feb10./programa_prueba.c
@@ -0,0 +1,9 @@
+/*Creado el 10 de Febrero de 2025 por Brithney Guadalupe Macias Rivera*/ 
+#include<stdio.h> 
+int main() 
+{ 
+ 
+printf("Buen dia humano \n"); 
+printf("Nunca olvides desayunar\n"); 
+} 
+/*Termine de escribir el programa*/
