e601b86d6e48c7e5fdded1657db6a4fdc2838014
Brithney_riv <bg.maciasrivera@ugto.mx>
2025-03-19 01:33:31 -0600

    Tarea del 19 de febrero corregida en clase

--- Feb24/Tarea_ejercicio.c
+++ Feb24/Tarea_ejercicio.c
@@ -1,37 +1,63 @@
-/* RevisiÛn del ejercicio 19 de Febrero, escrito el 24 de Febrero*/
-
-/*Conversion de coordenadas cartesianas a esfericas y cili≠ndricas*/
-#include <stdio.h>
-# include <math.h>
-
- int main()
-{
-    float x, y, z; //Coordenadas Cartesianas
-    float r, theta, phi; //Coordenadas esfÈricas salida
-    float rc, theta_c, zc; //Coordenadas cilindricas salida
+/* Revisi√≥n del ejercicio 19 de Febrero, escrito el 24 de Febrero*/
  
-  //Instrucciones al usuario 
-  
-   printf("Conversion de coordenadas cartesianas a esfericas y clindricas\n");
-   printf("Introduce los valores de las variables x, y, z que quieres convertir \n");
-   scanf("%f", &x);
-   scanf("%f", &y);
-   scanf("%f", &z);
-  
-   //Realizo las operaciones de transformaciÛn de coordenadas
-   //Operaciones para esfericas
-    r = sqrt(x*x+y*y);
-    theta = atan(y/x);
-    phi = acos(z/x);
  
-   //Operaciones para cilindricas
-    rc = sqrt(x*x+y*y);
-    theta_c = atan(x/y);
-    zc = z;
+ #include <stdio.h>
+ #include <math.h>
+ 
+ int main(){
+ 
+ /*Conversion de coordenadas cartesianas a esfericas y cili¬≠ndricas*/
+ #include <stdio.h>
+ # include <math.h>
+ 
+  int main()
+ {
+     float x, y, z; //Coordenadas Cartesianas
+     float r, theta, phi; //Coordenadas esf√©ricas salida
+     float rc, theta_c, zc; //Coordenadas cilindricas salida
+ 
+    
+     printf("Introduce los valores de las variables x, y, z que quieres convertir \n");
+     scanf("%f", &x);
+     scanf("%f", &y);
+     scanf("%f", &z);
+  
+     //Realizo las operaciones de transformaci√≥n de coordenadas
+     //Coordenadas esf√©ricas
+     float rc, theta_c, zc; //Coordenadas cilindricas salida
+  
+   //Instrucciones al usuario 
    
-   //Salida
-   printf("Las coordenadas esfericas obtenidas son r=%f, theta=%f, phi=%f  \n", r, theta, phi);
-   printf("Las coordenadas cilindricas obtenidas son rc=%f, theta_c=%f, z=%f  \n", rc, theta_c, zc);
-   // exit(0);
+    printf("Conversion de coordenadas cartesianas a esfericas y clindricas\n");
+    printf("Introduce los valores de las variables x, y, z que quieres convertir \n");
+    scanf("%f", &x);
+    scanf("%f", &y);
+    scanf("%f", &z);
    
-}
+    //Realizo las operaciones de transformaci√≥n de coordenadas
+    //Operaciones para esfericas
+     r = sqrt(x*x+y*y);
+     theta = atan(y/x);
+     phi = acos(z/x);
+ 
+     //Coordenadas cilindricas
+     phi = acos(z/x);
+  
+    //Operaciones para cilindricas
+     rc = sqrt(x*x+y*y);
+     theta_c = atan(x/y);
+     zc = z;
+     zc = z;
+    
+    //Salida
+    printf("Las coordenadas esfericas obtenidas son r=%f, theta=%f, phi=%f  \n", r, theta, phi);
+    printf("Las coordenadas cilindricas obtenidas son rc=%f, theta_c=%f, z=%f  \n", rc, theta_c, zc);
+    // exit(0);
+ 
+     //Salida
+     printf("Las coordenadas esfericas son r=%f, theta=%f, phi=%f  \n", r, theta, phi);
+     printf("Las coordenadas cilindricas son rc=%f, theta_c=%f, z=%f  \n", rc, theta_c, zc);
+    // exit(0);
+ }
+  
+ }
