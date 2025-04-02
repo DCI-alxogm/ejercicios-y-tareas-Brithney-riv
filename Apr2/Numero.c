514371d4d57a528589815545c0cf61dff82c1a46
Brithney-riv <bg.maciasrivera@ugto.mx>
2025-04-02 11:39:15 -0600

    Ejercicio 2 de clase

--- Apr2/Numero.c
+++ Apr2/Numero.c
@@ -1,29 +1,70 @@
 #include <stdio.h>
- 
+#include <math.h>
+
 int main() {
- 
-    int inicio, fin, incremento, x;
- 
-    // Solicitar al usuario los valores de las variables 
-    printf("Introduce el valor inicial, final e incremento  de X:\n ");
-    scanf("%d %d %d", &inicio, &fin, &incremento);
- 
-    // Comprobamos que el incremento no sea un número igual a 0
-    if (incremento == 0) {
-        printf("El incremento no puede ser cero.\n");
-        return 1; 
-    }
- 
-    x = inicio;
- 
-    // Utilizando  do...while 
-    do {
-        // Calcular el valor de la función x^2
-        printf("X: %d, su cuadrado es: %d\n", x, x * x);
- 
-        x += incremento;
- 
-    } while (x <= fin); 
- 
+    int inicio, fin;
+
+    do { 
+        // Solicitar intervalo
+        printf("Ingresa el valor inicial del intervalo: ");
+        scanf("%d", &inicio);
+        printf("Ingresa el valor final del intervalo: ");
+        scanf("%d", &fin);
+
+        if (inicio > fin) {
+            printf("El valor inicial debe ser menor o igual al final. Intenta de nuevo.\n");
+            continue; 
+        }
+
+        int primos[100]; 
+        int contador = 0;
+
+        int i = inicio;
+        do {
+            // Verificar si el número es primo
+            int esPrimo = 1; 
+            if (i < 2) {
+                esPrimo = 0; 
+            } else {
+                int j = 2;
+                do {
+                    if (i % j == 0) {
+                        esPrimo = 0; // No es primo
+                        break;
+                    }
+                    j++;
+                } while (j * j <= i);
+            }
+
+            if (esPrimo) {
+                primos[contador++] = i;
+                if (contador >= 100) {
+                    break; // Limite de 100 números primos
+                }
+            }
+            i++;
+        } while (i <= fin);
+
+        if (contador > 100) {
+            printf("El intervalo contiene más de 100 números primos. Ingresa un intervalo más pequeño.\n");
+        } else {
+            // Imprimir los números primos en renglones de 20 números máximo
+            printf("Números primos en el intervalo:\n");
+            int linea = 0;
+            int i = 0;
+            do {
+                printf("%d ", primos[i]);
+                linea++;
+                if (linea == 20) { // Imprimir 20 números por línea
+                    printf("\n");
+                    linea = 0;
+                }
+                i++;
+            } while (i < contador);
+            printf("\n");
+            break; // Salir si se cumplen todas las condiciones
+        }
+    } while (1); // Repetir hasta que se cumplan las condiciones
+
     return 0;
 }
